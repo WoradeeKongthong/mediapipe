f10c5120bfe0c3097b01ec40fca311f1b0019726
WoradeeKongthong <woradee.s@gmail.com>
2021-10-04 11:28:33 +0700

    add code

---
+++ face_detection_on_video_with_mediapipe.py
@@ -0,0 +1,41 @@
+"""
+Face Detection on video with MediaPipe
+"""
+import cv2
+import mediapipe as mp
+
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture('data/people.mp4')
+
+while cap.isOpened():
+    check, img = cap.read()
+    if check :
+        img = cv2.resize(img, (int(img.shape[1]/5), int(img.shape[0]/5)))
+        
+        # face detection
+        with mpFaceDetection.FaceDetection(model_selection=1, min_detection_confidence=0.5) as detector :
+            results = detector.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            
+            if results.detections :
+                for detection in results.detections:
+                    # extract bounding box from detection
+                    bboxD = detection.location_data.relative_bounding_box
+                    ih, iw, ic = img.shape
+                    x, y, w, h = int(bboxD.xmin * iw), int(bboxD.ymin * ih),\
+                        int(bboxD.width * iw), int(bboxD.height * ih)
+            
+                    # draw detections
+                    # option 1
+                    mpDraw.draw_detection(img, detection)
+                    # option 2
+                    # cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,255), 1)
+                
+        cv2.imshow("result", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
--- face_detection_with_mediapipe.py
+++ face_detection_with_mediapipe.py
@@ -27,7 +27,7 @@ with mpFaceDetection.FaceDetection(
             # extract bounding box from detection
             bboxD = detection.location_data.relative_bounding_box
             ih, iw, ic = annotated_img.shape
-            bbox = int(bboxD.xmin * iw), int(bboxD.ymin * ih),\
+            x, y, w, h = int(bboxD.xmin * iw), int(bboxD.ymin * ih),\
                 int(bboxD.width * iw), int(bboxD.height * ih)
             
             # extract landmarks (ight eye, left eye, nose tip, mouth center, 
@@ -35,7 +35,10 @@ with mpFaceDetection.FaceDetection(
             lm = detection.location_data.relative_keypoints
             
             # draw detection on image
+            # option 1 
             mpDrawer.draw_detection(annotated_img, detection)
+            # option 2
+            # cv2.rectangle(annotated_img, (x,y), (x+w, y+h), (255,255,255), 1)
 
 cv2.imshow("press q to exit", annotated_img)
 cv2.waitKey(0)
